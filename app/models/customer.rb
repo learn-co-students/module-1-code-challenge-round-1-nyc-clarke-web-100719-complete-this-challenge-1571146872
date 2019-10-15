27f219392bdfc76dff8a490107d092a794e29cb7
jastaffen <jack.staffen@gmail.com>
2019-10-15 10:30:26 -0400

    really done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,49 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.customer == self }
+  end
+
+
+  # customer, restaurant, rating, content
+  def add_review(restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    reviews.length
+  end
+
+  def restaurants
+    reviews.map { |review| review.restaurant}.uniq
+  end
+  
+  def self.find_by_name(name)
+    @@all.find { |customer| customer.full_name == name }
+  end
+
+  def self.all_names
+    @@all.map { |customer| customer.full_name }
+  end
+
+  def self.find_all_by_first(first_name_passed_in)
+    @@all.select { |customer| customer.first_name == first_name_passed_in }
+  end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,48 @@
 class Restaurant
   attr_reader :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select { |review| review.restaurant == self}
+  end
+
+  # #added more methods in here ====================
+  # def bad_reviews
+  #   reviews.select { |review| review.rating <= 3 }
+  # end
+
+  # def good_reviews
+  #   reviews.select { |review| review.rating > 3 }
+  # end
+  # #===========================================  
+  def customers
+    reviews.map { |review| review.customer }.uniq
+  end
+
+  def total_rating
+    reviews.reduce(0) { |acc, review| acc + review.rating }
+  end
+
+  def average_star_rating
+    avg = (total_rating.to_f / reviews.length).round(2)
+  end
+
+  def longest_review
+    content_arr = reviews.sort_by { |review| review.content.length }
+    content_arr[-1].content
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |restaurant| restaurant.name == name }
   end
 
 end
