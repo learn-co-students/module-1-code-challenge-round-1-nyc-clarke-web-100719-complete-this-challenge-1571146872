0fe929982138128c22acd46df922884cef410057
jastaffen <jack.staffen@gmail.com>
2019-10-15 10:40:05 -0400

    made some extra methods for fun

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -15,15 +15,45 @@ class Restaurant
     Review.all.select { |review| review.restaurant == self}
   end
 
-  # #added more methods in here ====================
-  # def bad_reviews
-  #   reviews.select { |review| review.rating <= 3 }
-  # end
+  #added more methods in here ====================
+  def bad_reviews
+    reviews.select { |review| review.rating <= 3 }
+  end
 
-  # def good_reviews
-  #   reviews.select { |review| review.rating > 3 }
-  # end
-  # #===========================================  
+  def good_reviews
+    reviews.select { |review| review.rating > 3 }
+  end
+
+  def stars_based_on_average
+    if average_star_rating == 0
+      return "no reviews"
+    elsif average_star_rating < 2
+      return "*"
+    elsif average_star_rating < 3
+      return "**"
+    elsif average_star_rating < 4
+      return "***"
+    elsif average_star_rating < 5
+      return "****"
+    elsif average_star_rating == 5
+      return "*****"
+    end
+  end
+
+  def best_review
+    review_arr = reviews.sort_by { |review| review.rating }
+    review_arr.last
+  end
+
+  def worst_review
+    review_arr = reviews.sort_by { |review| review.rating }
+    review_arr.first
+  end
+
+  def sort_reviews
+    reviews.sort_by { |review| -review.rating }
+  end
+  #===========================================  
   def customers
     reviews.map { |review| review.customer }.uniq
   end
