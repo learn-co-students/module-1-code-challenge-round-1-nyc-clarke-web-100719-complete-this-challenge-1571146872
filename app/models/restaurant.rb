284a510b61dee7ea01cd90e74195cac324e39c9b
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-15 10:33:46 -0400

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,35 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    self.class.all << self
   end
 
+  def reviews
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def customers
+    reviews.map { |review| review.customer }.uniq
+  end
+
+  def average_star_rating
+    reviews.any? ? reviews.sum(&:rating) / reviews.count.to_f : 0.0
+  end
+
+  def longest_review
+    reviews.max_by { |review| review.content.length }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all.find { |restaurant| restaurant.name == name }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+r1 = Restaurant.new('BK')
+r2 = Restaurant.new("Mickey D's")
+r3 = Restaurant.new('Shake Shack')
+
+c1 = Customer.new('Jackie', 'Chan')
+c2 = Customer.new('Stephen', 'Hawking')
+c3 = Customer.new('Barack', 'Obama')
+c4 = Customer.new('Jackie', 'Robinson')
+
+rv1 = c1.add_review(r1, 'Cheap and good', 4)
+rv2 = c2.add_review(r1, 'Yuck not my thing', 1)
+rv3 = c3.add_review(r1, 'This company is immoral', 3)
+rv4 = c1.add_review(r3, 'Quality but expensive', 3)
+rv5 = c1.add_review(r1, 'Changed my mind, found a rat', 1)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
