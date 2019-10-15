e38f7f15b7d6a4b5847a0b9e0bf35f5a808ab0eb
github username <github email address>
2019-10-15 10:37:43 -0400

    nickjardine done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,47 @@
 class Restaurant
+  
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews 
+    Review.all.select { |review| review.restaurant == self }
+  end
+
+  def customers
+    reviews.map { |review| review.customer }.uniq
+  end
+
+  def star_rating 
+    reviews.map { |review| review.rating }
+  end
+  
+  def average_star_rating
+    total = star_rating.reduce(0) { |sum, star_rating| sum + star_rating }
+    total / star_rating.length.to_f
+  end
+
+  def longest_review
+    content = reviews.map { |review| review.content}
+    content.max_by { |content| content.length }
+  end
+
+  def self.find_by_name(name)
+    all.find { |restaurant| restaurant.name == name }
+  end
+
+  def reviewer_first_names
+    reviews.map { |review| review.customer.first_name}        
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,26 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+c1 = Customer.new("nick", "jardine")
+c2 = Customer.new("pat", "jardine")
+c3 = Customer.new("rye", "jardine")
+c4 = Customer.new("heather", "schechter")
+c5 = Customer.new("heather", "jardine")
+
+rest1 = Restaurant.new("sonny's")
+rest2 = Restaurant.new("lilia")
+rest3 = Restaurant.new("best")
+rest4 = Restaurant.new("sonny's")
+
+rev1 = Review.new(c2, rest2, "good", 3)
+rev2 = Review.new(c1, rest3, "not best", 1)
+rev3 = Review.new(c4, rest3, "grand", 4) 
+rev4 = Review.new(c4, rest1, "great", 5)
+rev4 = Review.new(c4, rest1, "not great this time", 1)
+rev5 = Review.new(c3, rest3, "ok", 2)
+rev6 = Review.new(c3, rest2, "excellent", 5)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
