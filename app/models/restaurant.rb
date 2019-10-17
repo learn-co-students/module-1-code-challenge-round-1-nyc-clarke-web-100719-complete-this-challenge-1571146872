1016b1f5301c6837007d35426ea819b6379d2b6d
yoshinator <yoanante@gmail.com>
2019-10-17 13:20:46 -0400

    finished restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,31 @@ class Restaurant
     @@all << self 
   end
 
+  def customers 
+    reviews.map do |review|
+      review.customer
+    end.uniq
+  end 
+
+  def reviews 
+    Review.all.select do |review|
+      review.restaurant == self 
+    end 
+  end
+
+  def avg_star_rating
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating
+    end 
+    sum / reviews.length 
+  end
+
+  def longest_review
+    reviews.sort_by do |review|
+      review.content
+    end.last.content
+  end 
 
   #Begin class methods
   def self.all
